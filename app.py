7c8c68578f4f0fed2c77611be30137df3faf42cc
geethamaddu <gndlahari@gmail.com>
2026-03-10 15:41:02 -0400

    backend Code with ui updated

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
+import os
+
 import streamlit as st
+
+from rag_utility import process_document_ingestion,answer_question
+
+working_dir=os.getcwd()
+
+st.title("llama model Document RAG")
+
+uploaded_file=st.file_uploader("Upload a PDF file",type=["pdf"])
+
+if uploaded_file is not None:
+    save_path=os.path.join(working_dir,uploaded_file.name)
+    with open(save_path,"wb") as f:
+        f.write(uploaded_file.getbuffer())
+    process_document=process_document_ingestion(uploaded_file.name)
+    st.info("Document Processes Successfully")
+    user_question=st.text_area("Ask your question about the document")
+    if st.button("Answer"):
+        answer=answer_question(user_question)
+        st.markdown("llama response")
+        st.markdown(answer)
--- rag_utility.py
+++ rag_utility.py
@@ -11,3 +11,50 @@ from langchain_text_splitters import CharacterTextSplitter,RecursiveCharacterTex
 from langchain_groq import ChatGroq
 from langchain_classic.chains import RetrievalQA
 print("Imports successful")
+
+
+working_dir=os.path.dirname(os.path.abspath((__file__)))
+print("path loaded")
+
+embedding=HuggingFaceEmbeddings()
+print("embeding model loaded")
+
+llm=ChatGroq(
+    model="llama-3.3-70b-versatile",
+    temperature=0
+)
+print("llm model loaded")
+
+def process_document_ingestion(file_name):
+    loader=PyMuPDFLoader(f"{working_dir}/{file_name}")
+    documents=loader.load()
+    text_splitter=RecursiveCharacterTextSplitter(
+        chunk_size=2000,
+        chunk_overlap=500
+    )
+    texts=text_splitter.split_documents(documents)
+    vectordb=Chroma.from_documents(
+        documents=texts,
+        embedding=embedding,
+        persist_directory=f"{working_dir}/doc_vectorstore"
+    )
+    return 0
+print("process_document_ingestion successful")
+
+def answer_question(user_question):
+    vectordb=Chroma(
+        persist_directory=f"{working_dir}/doc_vectorstore",
+        embedding_function=embedding
+    )
+    retriever=vectordb.as_retriever()
+    qa_chain=RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=retriever
+    )
+    response=qa_chain.invoke({"query":user_question})
+    answer=response["result"]
+
+    return answer
+
+print("answer_question successful")
